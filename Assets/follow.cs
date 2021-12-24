1a24066259a0497bbd7c520b45ac5eb1a1d8a61a
Wickhi <81701556+Wickhi@users.noreply.github.com>
2021-12-25 00:10:28 +0100

    character switch

--- Assets/follow.cs
+++ Assets/follow.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class follow : MonoBehaviour
 {
-    public Transform player;
+    public Transform Stuart;
+    public Transform George;
+    public Transform John;
+    public int playervalue = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +18,29 @@ public class follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            playervalue = playervalue + 1;
+        }
+        if (playervalue > 3)
+        {
+            playervalue = 1;
+        }
+        if (playervalue == 1)
+        {
+            transform.position = new Vector3(Stuart.position.x, Stuart.position.y, transform.position.z);
 
+        }
+        if (playervalue == 2)
+        {
+            transform.position = new Vector3(George.position.x, George.position.y, transform.position.z);
+
+        }
+        if (playervalue == 3)
+        {
+            transform.position = new Vector3(John.position.x, John.position.y, transform.position.z);
+
+        }
         //baseangle < angle4 && angle4 >= 90;
         //angle3 = angle4 - baseangle;
         //angle3 = angle3 - change;
