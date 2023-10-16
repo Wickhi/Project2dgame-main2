94e0cf0096942fa3df8c4a0a757742db1bbe64e1
Wickhi <81701556+Wickhi@users.noreply.github.com>
2023-10-16 22:40:14 +0200

    Gridnavmesh update
    navmesh

---
+++ Assets/gridgenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridgenerator : MonoBehaviour
+{
+    // https://pavcreations.com/tilemap-based-a-star-algorithm-implementation-in-unity-game/
+    // https://pavcreations.com/pathfinding-with-a-star-algorithm-in-unity-small-game-project/
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
