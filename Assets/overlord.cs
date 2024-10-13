661fad22444e8946c91c7e92f126624e3a948475
Wickhi <81701556+Wickhi@users.noreply.github.com>
2024-10-13 22:49:48 +0200

    update 31

--- Assets/overlord.cs
+++ Assets/overlord.cs
@@ -11,7 +11,12 @@ public class overlord : MonoBehaviour
     public string Difficulty;
     public string DifficultyName;
     public int Difficultylevel;
-    public int Assaultlevel;
+    public int Assaultphase;
+    public bool activate;
+    public bool Reconphaseactive;
+    public GameObject ReconPrefab;
+    public bool returned;
+    public GameObject Recon;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +26,31 @@ public class overlord : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Assaultphase == 1)
+        {
+            //reconphaseStart
+            ReconphaseStart(ReconPrefab, Mormakspawnpoint);
+            if (Recon == null)
+            {
 
+            }
+        }
     }
     void spawnenemy(GameObject enemy, Vector3 spawnpoint)
     {
         Instantiate(enemy, spawnpoint, gameObject.transform.rotation);
 
     }
-    void Determineassaulttype()
+    void Determineassaulttype(GameObject enemy, Vector3 spawnpoint)
     {
 
     }
-    void Reconphase()
+    void ReconphaseStart(GameObject enemy, Vector3 spawnpoint)
+    {
+        Recon = Instantiate(enemy, spawnpoint, gameObject.transform.rotation);
+
+    }
+    void ReconphaseEnd(GameObject enemy, Vector3 spawnpoint)
     {
 
     }
